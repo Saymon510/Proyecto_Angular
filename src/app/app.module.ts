e4ccc688aa356699118fc55e5a344110a536c2ee
1754407938 <sav.mina@yavirac.edu.ec>
2022-11-28 09:54:49 -0500

    modulos y rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,20 @@ import { AppComponent } from './app.component';
 import { NopagesfoundComponent } from './pages/nopagesfound/nopagesfound.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { AppRoutingModule } from './app-routing.module';
+import { PagesModule } from './pages/pages.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
+    NopagesfoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,24 +4,29 @@ import { ProductComponent } from './product/product.component';
 import { CatalogueComponent } from './catalogue/catalogue.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { NopagesfoundComponent } from './nopagesfound/nopagesfound.component';
+import { PagesComponent } from './pages.component';
+import { SharedModule } from '../shared/shared.module';
+import { AppRoutingModule } from '../app-routing.module';
 
 
 
 @NgModule({
   declarations: [
-    ProductComponent,
-    CatalogueComponent,
-    DashboardComponent,
-    NopagesfoundComponent,
+  DashboardComponent,
+  ProductComponent,
+  CatalogueComponent,
+  PagesComponent,
   ],
   exports: [
-    ProductComponent,
-    CatalogueComponent,
-    DashboardComponent,
-    NopagesfoundComponent,
+  DashboardComponent,
+  ProductComponent,
+  CatalogueComponent,
+  PagesComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
+    AppRoutingModule,
   ]
 })
 export class PagesModule { }
