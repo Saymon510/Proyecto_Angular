88d33e2d6f531c5548a825c16f7eb3368814832d
1754407938 <sav.mina@yavirac.edu.ec>
2022-11-28 08:08:38 -0500

    modulos y componentes

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from './product/product.component';
 import { CatalogueComponent } from './catalogue/catalogue.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NopagesfoundComponent } from './nopagesfound/nopagesfound.component';
 
 
 
 @NgModule({
   declarations: [
     ProductComponent,
-    CatalogueComponent
+    CatalogueComponent,
+    DashboardComponent,
+    NopagesfoundComponent,
+  ],
+  exports: [
+    ProductComponent,
+    CatalogueComponent,
+    DashboardComponent,
+    NopagesfoundComponent,
   ],
   imports: [
     CommonModule
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    BreadcrumbsComponent
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    BreadcrumbsComponent
+  ],
   imports: [
     CommonModule
   ]
